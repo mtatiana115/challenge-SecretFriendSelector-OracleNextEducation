fad5dce383e920d1fb3721e0498ec064d00aa407
tatiana.maldonado <tatiana.maldonado@iesonline.com.co>
2025-02-07 12:15:56 -0500

    feat: :sparkles: Add friend input functionality with validation and dynamic list update

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 let friends = [];
 
+//Capture the input field value
+function addFriend() {
+    let friendName = document.getElementById("amigo").value.trim();
+
+    //validate the empty input
+    if (friendName === "") {
+        alert("Please enter a friend name");
+        return;
+    }
+    //ass the list of friends
+    friends.push(friendName);
+    //update and clear the input field
+    updateFriendsList();
+    document.getElementById("amigo").value = "";
+}
+
+function updateFriendsList() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    friends.forEach(function (friend) {
+        let li = document.createElement("li");
+        li.innerText = friend;
+        listaAmigos.appendChild(li);
+    });
+}
