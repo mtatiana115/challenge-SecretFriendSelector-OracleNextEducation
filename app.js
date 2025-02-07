ee6d1591164381ab18a08186777e36a2e9be24e9
tatiana.maldonado <tatiana.maldonado@iesonline.com.co>
2025-02-07 12:43:55 -0500

    feat: :sparkles: Add function to randomly select a friend from the array, with validation and result display

--- app.js
+++ app.js
@@ -2,26 +2,43 @@ let friends = [];
 
 //Capture the input field value
 function addFriend() {
-    let friendName = document.getElementById("amigo").value.trim();
+    let friendName = document.getElementById("friend").value.trim();
 
     //validate the empty input
     if (friendName === "") {
         alert("Please enter a friend name");
         return;
     }
-    //ass the list of friends
+    //add the list of friends
     friends.push(friendName);
     //update and clear the input field
     updateFriendsList();
-    document.getElementById("amigo").value = "";
+    document.getElementById("friend").value = "";
 }
 
 function updateFriendsList() {
-    let listaAmigos = document.getElementById("listaAmigos");
-    listaAmigos.innerHTML = "";
+    let friendList = document.getElementById("friendList");
+    friendList.innerHTML = "";
     friends.forEach(function (friend) {
         let li = document.createElement("li");
         li.innerText = friend;
-        listaAmigos.appendChild(li);
+        friendList.appendChild(li);
     });
 }
+
+//Sort the list of friends
+function drawSecretFriend() {
+
+    if (friends.length === 0) {
+        alert("Please add at least one friend");
+        return;
+    }
+
+    let randomIndex = Math.floor(Math.random() * friends.length);
+    let secretFriend = friends[randomIndex];
+    friends.splice(randomIndex, 1);
+
+    updateFriendsList();
+    document.getElementById("result").innerHTML = secretFriend;
+}
+
